92e3d3b5987ca1e63a5c6e107c9eba3c0e03f151
jdboy08 <justindoane@gmail.com>
2021-10-25 20:46:15 -0400

    added a footer

--- benchmark/src/App.js
+++ benchmark/src/App.js
@@ -1,31 +1,34 @@
-import logo from "./assets/logo.png";
+// import logo from "./assets/logo.png";
 import NavBar from "./components/NavBar";
 import "./App.css";
-import { Nav } from "react-bootstrap";
+// import { Nav } from "react-bootstrap";
 import { Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
+import { Footer } from "./components/index";
 // ?
 function App() {
   return (
     <div className="App">
       <NavBar />
 
-      <Switch>
-        {/* Contact Page */}
-        <Route path="/contact/:id">
-            
-        </Route>
+        <Switch>
+            {/* Contact Page */}
+            <Route path="/contact/:id">
+                
+            </Route>
 
-        <Route path="/contact">
-          <Contact />
-        </Route>
+            <Route path="/contact">
+            <Contact />
+            </Route>
 
-        {/* Main Page */}
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
+            {/* Main Page */}
+            <Route path="/">
+            <Home />
+            </Route>
+        </Switch>
+
+      <Footer />
     </div>
   );
 }
---
+++ benchmark/src/components/index.js
@@ -0,0 +1,2 @@
+export {default as NavBar} from './NavBar.js';
+export {default as Footer} from './Footer.js';

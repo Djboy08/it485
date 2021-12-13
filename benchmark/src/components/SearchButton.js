d4cdaefb103230a4c9a90a8515e7af3107926bea
jdboy08 <justindoane@gmail.com>
2021-12-13 00:07:18 -0500

    turned off autocomplete

--- benchmark/src/components/SearchButton.js
+++ benchmark/src/components/SearchButton.js
@@ -2,71 +2,66 @@ import gamesList from "../assets/js/gamesList.json";
 import React, { useState, useEffect } from "react";
 
 function SearchButton(props) {
+  const [gameValue, setGameValue] = useState("");
+  const [gamesL, setGamesL] = useState(gamesList);
+  const menuItemsRef = React.useRef();
+  const noItems = React.useRef();
 
-    const [gameValue, setGameValue] = useState("");
-    const [gamesL, setGamesL] = useState(gamesList);
-    const menuItemsRef = React.useRef();
-    const noItems = React.useRef();
-  
-    useEffect(() => {
-      //   setGamesL(gamesList);
-      // menuItemsRef.current. = buildDropDown(gamesList.data);
+  useEffect(() => {
+    //   setGamesL(gamesList);
+    // menuItemsRef.current. = buildDropDown(gamesList.data);
+  });
+
+  function checkForGame(e) {
+    const str = e.target.value;
+    setGameValue(str);
+    if (str == "" || str == null || str == " ") {
+      setGamesL(gamesList);
+      return;
+    }
+    const lowerCaseStr = str.toLowerCase();
+    const results = gamesList.data.filter((game) => {
+      return (
+        game.substring(0, lowerCaseStr.length).toLowerCase() == lowerCaseStr
+      );
     });
-  
-    function checkForGame(e) {
-      const str = e.target.value;
-      setGameValue(str);
-      if (str == "" || str == null || str == " ") {
-        setGamesL(gamesList);
-        return;
-      }
-      const lowerCaseStr = str.toLowerCase();
-      const results = gamesList.data.filter((game) => {
-        return (
-          game.substring(0, lowerCaseStr.length).toLowerCase() == lowerCaseStr
-        );
-      });
-      let d = {
-        data: results,
-      };
-  
-      if(results.length === 0){
-        noItems.current.style.display = "block";
-      }else{
-          noItems.current.style.display = "none";
-      }
-      console.log(results);
-      setGamesL(d);
-    }
-  
-    function buildDropDown() {
-      const group = gamesL.data.map((gameName) => {
-        const element = (
-          <input
-            type="button"
-            class="dropdown-item"
-            type="button"
-            value={gameName}
-            onClick={(e) => {
-              document.querySelector("#menu").style.display = "none";
-              setGameValue(e.target.value);
-            }}
-          />
-        );
-  
-        return element;
-      });
-      return group;
-    }
-    //   buildDropDown(names);
-  
-    function onClick(e) {
-      e.preventDefault();
-      console.log("test");
-      document.querySelector("#menu").style.display = "block";
-    }
+    let d = {
+      data: results,
+    };
 
+    if (results.length === 0) {
+      noItems.current.style.display = "block";
+    } else {
+      noItems.current.style.display = "none";
+    }
+    setGamesL(d);
+  }
 
+  function buildDropDown() {
+    const group = gamesL.data.map((gameName) => {
+      const element = (
+        <input
+          type="button"
+          class="dropdown-item"
+          type="button"
+          value={gameName}
+          onClick={(e) => {
+            document.querySelector("#menu").style.display = "none";
+            setGameValue(e.target.value);
+          }}
+        />
+      );
+
+      return element;
+    });
+    return group;
+  }
+  //   buildDropDown(names);
+
+  function onClick(e) {
+    e.preventDefault();
+    document.querySelector("#menu").style.display = "block";
+  }
 
   return (
     <>
@@ -94,6 +89,7 @@ function SearchButton(props) {
               id="searchCoin"
               placeholder="Search game"
               autofocus="autofocus"
+              autoComplete="off"
               onChange={checkForGame}
               value={gameValue}
             />
